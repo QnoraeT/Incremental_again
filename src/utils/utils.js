a902406d76d37bbb5cf39733ad433626b192752a
QnoraeT <xxajlopezxx@gmail.com>
2023-03-21 20:12:18 -0400

    some optimizations and stuff
    how do i optimize this dumb html code i made

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,6 @@
 function format(number, fixed = false)
 {
-    const abbSuffixes = ["","K","M","B","T","Qa","Qi","Sx","Sp","O","N","D"]
+    const abbSuffixes = ["","K","M","B","T","Qa","Qi","Sx","Sp","Oc","No","Dc","UDc","DDc","TDc","QaDc","QiDc","SxDc","SpDc","OcDc","NoDc","Vg"]
 
     if (number.lessThan(1_000_000))                                         //display with commas
         return Math.floor(number.toNumber()).toLocaleString("en-US");
@@ -15,7 +15,7 @@ function format(number, fixed = false)
         return  Number.parseFloat(mantissa.toFixed(3)) + abbSuffixes[powerOf1000];
     }
 
-                                                            //display in scientific notation
+    //display in scientific notation
     let exponent = Decimal.floor(number.log10());
     let mantissa = number.divide(Decimal.pow(10, exponent));
 
