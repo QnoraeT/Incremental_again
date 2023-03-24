ecef98625f177f6c5ea4bb5f5c8b46c15eb3ab1d
Yoreni <yoreni@icloud.com>
2023-03-24 22:43:32 +0100

    added  edge cases to the format function
    such as handling negitive numbers and when it is infinite

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,27 +3,35 @@ function format(number, fixed = false)
     const abbSuffixes = ["","K","M","B","T","Qa","Qi","Sx","Sp","Oc","No","Dc",
                         "UDc","DDc","TDc","QaDc","QiDc","SxDc","SpDc","OcDc","NoDc","Vg"];
 
+
+    if (number.lt(0))
+      return "-" + format(number.times(-1), fixed)
+    if (!Number.isFinite(points.mag))
+      return "Eternity"
+    if (Number.isNaN(points.mag))
+      return "NaN"
+
     if (number.lessThan(1_000_000))                                         //display with commas
-        return Math.floor(number.toNumber()).toLocaleString("en-US");
+      return Math.floor(number.toNumber()).toLocaleString("en-US");
     if (number.lessThan(new Decimal("1e" + (abbSuffixes.length * 3))))           //display abberviaed
     {
-        let powerOf1000 = Decimal.floor(number.log(1000));
-        let mantissa = number.divide(Decimal.pow(1000, powerOf1000))
-        
-        if (mantissa.round() >= 1000)
-          mantissa = 999.999;
+      let powerOf1000 = Decimal.floor(number.log(1000));
+      let mantissa = number.divide(Decimal.pow(1000, powerOf1000))
 
-        if (fixed)
-          return mantissa.toFixed(3) + abbSuffixes[powerOf1000];
-        return  Number.parseFloat(mantissa.toFixed(3)) + abbSuffixes[powerOf1000];
-    }
+      if (mantissa.round() >= 1000)
+        mantissa = 999.999;
 
-    //display in scientific notation
-    let exponent = Decimal.floor(number.log10());
-    let mantissa = number.divide(Decimal.pow(10, exponent));
+      if (fixed)
+        return mantissa.toFixed(3) + abbSuffixes[powerOf1000];
+      return  Number.parseFloat(mantissa.toFixed(3)) + abbSuffixes[powerOf1000];
+  }
 
-    if (fixed)
-        return mantissa.toFixed(3) + "e" + exponent;
+  //display in scientific notation
+  let exponent = Decimal.floor(number.log10());
+  let mantissa = number.divide(Decimal.pow(10, exponent));
+
+  if (fixed)
+    return mantissa.toFixed(3) + "e" + exponent;
     return Number.parseFloat(mantissa.toFixed(3)) + "e" + exponent;
 
 }
